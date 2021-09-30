9adbd066f19f62c2f3e41fed880af4abfd83ee7c
Ersel Metz Magbanua <magbanuaersel@gmail.com>
2021-09-30 23:14:26 +0800

    initial commit

--- js/core.js
+++ js/core.js
@@ -1,30 +1,26 @@
 $(window).on('load',function(){
     $('main').load('home.html');
-    $('.home').addClass('is-active gradient');
+    $('.home').addClass('text-red');
 });
 
 $(document).ready(function(){
     $('.home').on('click',function(e){
         e.preventDefault();
-        $('.project, .about').removeClass('is-active gradient');
-        $('.home').addClass('is-active gradient');
+        $('.project, .about').removeClass('text-red');
+        $('.home').addClass('text-red');
         $('main').load('home.html');
     });
     $('.project').on('click',function(e){
         e.preventDefault();
-        $('.home, .about').removeClass('is-active gradient');
-        $('.project').addClass('is-active gradient');
+        $('.home, .about').removeClass('text-red');
+        $('.project').addClass('text-red');
         $('main').load('project.html');
     });
     $('.about').on('click',function(e){
         e.preventDefault();
-        $('.home, .project').removeClass('is-active gradient');
-        $('.about').addClass('is-active gradient');
+        $('.home, .project').removeClass('text-red');
+        $('.about').addClass('text-red');
         $('main').load('about.html');
-    });
-
-    $('.navbar-burger').click(function(){
-        $('.navbar-menu').toggle('is-active');
     });
     $('.navbar-trigger').click(function(e){
         e.preventDefault();
