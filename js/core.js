2aec0d21532b6531fe3f540549b202a6f7cc52c0
Ersel Metz Magbanua <65773510+erselmetz@users.noreply.github.com>
2022-01-02 14:24:55 +0800

    Add files via upload

--- js/core.js
+++ js/core.js
@@ -1,146 +1,157 @@
-import Metz from './metz.js';
-
-let $ = new Metz();
-
-var main = document.getElementById('main');
-var home = document.querySelector('.home');
-var project = document.querySelector('.project');
-var about = document.querySelector('.about');
-var navbar = document.querySelector('#navbar');
+var main = document.getElementById('main')
+var home = document.querySelector('.home')
+var project = document.querySelector('.project')
+var about = document.querySelector('.about')
+var navbar = document.querySelector('#navbar')
+var navbarTrigger = document.querySelector('.w3-navbar-trigger')
 
 function sendMail(){
 
-    var from_name = document.getElementById('from_name');
-    var from_email = document.getElementById('from_email');
-    var message = document.getElementById('message');
-    var message_status = document.querySelector('.message-status');
+    var from_name = document.getElementById('from_name')
+    var from_email = document.getElementById('from_email')
+    var message = document.getElementById('message')
+    var message_status = document.querySelector('.message-status')
 
-    message_status.innerHTML = 'Sending!!';
+    message_status.innerHTML = 'Sending!!'
 
     var tempParams = {
         from_name: from_name.value,
         from_email: from_email.value,
         message: message.value,
-    };
+    }
 
     emailjs.send('service_mym8wyj','template_zgc8t6b',tempParams)
     .then(function(res){
-        console.log('success',res.status);
-        from_name.value = '';
-        from_email.value = '';
-        message.value = '';
-        message_status.innerHTML = "&#10003; message sent!!";
+        console.log('success',res.status)
+        from_name.value = ''
+        from_email.value = ''
+        message.value = ''
+        message_status.innerHTML = "&#10003 message sent!!"
     },function(error){
-        alert('Error Sending Message!!',error);
-    });
+        alert('Error Sending Message!!',error)
+    })
 }
 
 function loadHome(){
     fetch('home.html').then((response)=>{
-        return response.text();
+        return response.text()
     }).then(function(data){
-        main.innerHTML = data;
+        main.innerHTML = data
 
-        $.animateText({
+        $('#sub').animateText({
             text: 'Backend Web Developer',
-            id: 'sub',
-        },100);
+        },100)
 
-        $.animateColor({
+        $('#card').animateColor({
             color: 'random',
-            id: 'card',
             type:'border'
-        },1000);
+        },1000)
 
-        $.animateColor({
+        $('.w3-title').animateChar({
+            text: 'Hi, I am Ersel Metz Magbanua, 17 years old.',
+            effect: 'top',
             color: 'random',
-            id: 'sub',
-            type:'text'
-        },500);
+            speed: 40
+        })
 
-        $.counterUp({
-            number:'17',
-            id:'age',
-        },100);
-    });
+    })
 }
 
 function loadProject(){
     fetch('project.html').then((response)=>{
-        return response.text();
+        return response.text()
     }).then(function(data){
-        main.innerHTML = data;
+        main.innerHTML = data
 
-        $.counterUp({
+        $('#project').counterUp({
             number: 5,
-            id: 'project',
-            loop: false,
-        },100);
+        },100)
 
-        $.animateColor({
-            color: 'random',
-            id: 'project',
-            type: 'text'
-        },100);
-    });
+        // $('#project').animateColor({
+        //     color: ['white','black'],
+        //     type: 'text',
+        // },1000)
+
+        var image = document.querySelectorAll('.image')
+        var imageShow = document.querySelector('.imageShow')
+        var imageShowModal = document.querySelector('.imageShowModal')
+        var hideModal = document.getElementsByClassName('hideModal')
+
+        hideModal.position = 'fixed'
+
+        var clickImage = (e)=>{
+            imageShowModal.style.display = 'block'
+            imageShow.src = e.target.src
+            imageShow.width = innerWidth - 50
+            imageShow.height = innerHeight - 20
+        }
+
+        var clickHideModalButton = (e)=>{
+            imageShowModal.style.display = 'none'
+        }
+
+        for(var i = 0; i < image.length; i++){
+            image[i].addEventListener('click',clickImage,false)
+        }
+
+        for(var i = 0; i<hideModal.length; i++){
+            hideModal[i].addEventListener('click',clickHideModalButton,false)
+        }
+
+    })
 }
 
 function loadAbout(){
     fetch('about.html').then((response)=>{
 
-        return response.text(); 
+        return response.text() 
 
     }).then((data)=>{
 
-        main.innerHTML = data;
+        main.innerHTML = data
 
-        var sendEmail = document.getElementById('sendEmail');
+        var sendEmail = document.getElementById('sendEmail')
         sendEmail.addEventListener('click',()=>{
-            sendMail();
-        });
+            sendMail()
+        })
 
-    });
+    })
 }
 
 window.addEventListener('load',function(){
-    home.classList.add('text-red');
+    home.classList.add('w3-text-red')
 
-    loadHome();
+    loadHome()
 
-});
+})
 
 home.addEventListener('click',function(){
-    about.classList.remove('text-red');
-    project.classList.remove('text-red');
-    home.classList.add('text-red');
-    loadHome();
-});
+    about.classList.remove('w3-text-red')
+    project.classList.remove('w3-text-red')
+    home.classList.add('w3-text-red')
+    loadHome()
+})
 project.addEventListener('click',function(){
-    about.classList.remove('text-red');
-    home.classList.remove('text-red');
-    project.classList.add('text-red');
-    loadProject();
-});
+    about.classList.remove('w3-text-red')
+    home.classList.remove('w3-text-red')
+    project.classList.add('w3-text-red')
+    loadProject()
+})
 about.addEventListener('click',function(){
-    project.classList.remove('text-red');
-    home.classList.remove('text-red');
-    about.classList.add('text-red');
-    loadAbout();
-});
-document.querySelector('.navbar-trigger').addEventListener('click',function(){
-    navbar.classList.toggle('hide-small');
-});
+    project.classList.remove('w3-text-red')
+    home.classList.remove('w3-text-red')
+    about.classList.add('w3-text-red')
+    loadAbout()
+})
+navbarTrigger.addEventListener('click',function(){
+    navbar.classList.toggle('w3-hide-small')
+})
 home.addEventListener('click',function(){
-    navbar.classList.add('hide-small');
-});
+    navbar.classList.add('w3-hide-small')
+})
 project.addEventListener('click',function(){
-    navbar.classList.add('hide-small');
-});
+    navbar.classList.add('w3-hide-small')
+})
 about.addEventListener('click',function(){
-    navbar.classList.add('hide-small');
-});
-
-function modal_image(link){
-    $('.modal').show();
-    $('#image_here').attr('src',link);
-}
+    navbar.classList.add('w3-hide-small')
+})
--- js/metz.js
+++ js/metz.js
@@ -1,78 +1,228 @@
-export default class Metz{
-    animateColor(data,speed){
-        var obj = document.getElementById(data.id);
-        var x = 0;
-        var randomColor = [
-            'yellow',
-            'red',
-            'purple',
-            'green',
-            'yellowgreen',
-            'blue',
-            'cyan',
-            'orange',
-            'pink',
-        ];
+const strings ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+const numbers = '1234567890'
+const special = ',./@{}[]()%$#!*_-=+|/\\?<>'
+
+function generateString(length){
+    const characters = strings+numbers
+    result = ''
+    for(let i = 0; i < length; i++){
+        result += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    return result
+}
+
+function generateStringWithoutNumbers(length){
+    result = ''
+    for(let i = 0; i < length; i++){
+        result += strings.charAt(Math.floor(Math.random() * strings.length))
+    }
+    return result
+}
+
+function generateNumber(length){
+    result = ''
+    for(let i = 0; i < length; i++){
+        result += numbers.charAt(Math.floor(Math.random() * numbers.length))
+    }
+    return result
+}
+
+function randomColor() {
+    result = "#"+Math.floor(Math.random() * 16777215).toString(16)
+    return result
+}
+
+
+
+var $ = function(selector) {
+    if (! (this instanceof $) ) {
+        return new $(selector)
+    }
+    this.el = document.querySelectorAll(selector)
+    return this
+}
+
+$.prototype.css = function(prop, val){
+    this.el.forEach(function(element) {
+        element.style[prop] = val
+    })
+    return this
+}
+
+$.prototype.animate = function (params) {
+    
+}
+
+$.prototype.addClass = function (params) {
+    this.el.forEach(element => {
+        element.classList.add = params
+    });
+    return this
+}
+
+$.prototype.removeClass = function (params) {
+    this.el.forEach(element => {
+        element.classList.remove = params
+    });
+    return this
+}
+
+$.prototype.animateColor = function(data,speed){
+    var x = 0
+    this.el.forEach(obj => {
         if(data.color == 'random'){
-            data.color = randomColor;
+            setInterval(function(){obj.style.borderColor = randomColor()},speed)
         }
         if(data.type == 'border'){
-            obj.style.border = '1px solid';
-            setInterval(()=>{
+            obj.style.border = '1px solid'
+            setInterval(function(){
                 if(x <= data.color.length){
-                    obj.style.borderColor = data.color[x];
-                    x++;
+                    obj.style.borderColor = data.color
+                    x++
                 }
                 if(x > data.color.length){
-                    x = 0;
+                    x = 0
                 }
-            },speed);
+            },speed)
         }
         if(data.type == "text"){
-            setInterval(()=>{
+            setInterval(function(){
                 if(x <= data.color.length){
-                    obj.style.color = data.color[x];
-                    x++;
+                    obj.style.color = data.color[x]
+                    x++
                 }
                 if(x > data.color.length){
-                    x = 0;
+                    x = 0
                 }
-            },speed);
+            },speed)
         }
-    }
-    animateText(data,speed){
-        var animate = data;
-        var sub = document.getElementById(animate.id);
-        var text = animate.text += '       ';
-        var x = 1;
-        sub.innerHTML = animate.text.charAt(0);
-        setInterval(()=>{
+    })
+    return this
+}
+    
+$.prototype.animateText = function(data,speed){
+    var animate = data
+    var text = animate.text
+    var x = 1
+    this.el.forEach(function(sub){
+        sub.innerHTML = animate.text.charAt(0)
+        var timer = setInterval(function(){
             if(x <= text.length){
-                sub.innerHTML += text.charAt(x);
-                x++;
+                sub.innerHTML += text.charAt(x)
+                x++
             }
             if(animate.loop == true){
                 if(x >= text.length){
-                    x = 1;
-                    sub.innerHTML = animate.text.charAt(0);
+                    x = 1
+                    sub.innerHTML = animate.text.charAt(0)
                 }
             }
-        },speed);
-    }
-    counterUp(data,speed){
-        var count = data;
-        var x = 0;
-        var p = document.getElementById(count.id);
+            if(x >= text.length){
+                clearInterval(timer)
+            }
+        },speed)
+    })
+    return this
+}
+
+$.prototype.animateChar = function(data){
+    this.el.forEach(obj => {
+        var char = 0 
+        var text = obj.innerText
+        if(data.text){text = data.text}else{text = obj.innerText}
+        var splitText = text.split('')
+        obj.innerHTML = ''
+        const classCode = generateStringWithoutNumbers(8)
+
+        for(var i = 0;i < splitText.length;i++){
+            if(data.color && data.color == 'random'){
+                color = randomColor()
+            }else if(data.color != ''){
+                color = data.color
+            }else{
+                color = 'black'
+            }
+            obj.innerHTML += `<span class="${classCode} w3-invisible" style="color:${color}">${splitText[i]}</span>`
+        }
+
+        var timer = setInterval(function(){
+            var span = document.querySelectorAll('span.'+classCode)[char]
+            span.classList.remove('w3-invisible')
+            span.classList.add('w3-animate-'+data.effect)
+            char++
+            if(char >= splitText.length){
+                clearInterval(timer)
+            }
+        }, data.speed)
+    })
+    return this
+}
+
+$.prototype.counterUp = function(data,speed){
+    var count = data
+    var x = 0
+    this.el.forEach(obj => {
         setInterval(()=>{
             if(x <= count.number ){
-                p.innerHTML = x;
-                x++;
+                obj.innerHTML = x
+                x++
             }
             if(count.loop == true){
                 if(x >= count.number){
-                    x = 0;
+                    x = 0
                 }
             }
-        },speed);
-    }
+        },speed)
+    })
+    return this
+}
+
+$.prototype.on = function(param,callback) {
+    this.el.forEach(element => {
+        element.addEventListener(param,function(){
+            callback()
+        })
+    });
+    return this
+}
+
+$.prototype.focus = function (params) {
+    this.el.forEach(element => {
+        element.focus()
+    });
+    return this
+}
+
+$.prototype.text = function (params) {
+    this.el.forEach(element => {
+        if(params || params == ''){
+            element.innerText = params
+        }
+    });
+    return this.el[0].innerText
+}
+
+$.prototype.html = function (params) {
+    this.el.forEach(element => {
+        if(params || params == ''){
+            element.innerHTML = params
+        }
+    });
+    return this.el[0].innerHTML
+}
+
+$.prototype.val = function (params) {
+    this.el.forEach(element => {
+        if(params || params == ''){
+            element.value = params
+        }
+    });
+    return this.el[0].value
+}
+
+$.prototype.validate = function (params) {
+    this.el.forEach(element => {
+        
+    });
+    return this
 }
