91db0f33b4adbe7bb3390d339758ebf5ab70d968
erselmetz <magbanuaersel@gmail.com>
2022-04-25 13:30:25 +0800

    Update Project

--- core.js
+++ core.js
@@ -1,174 +1,194 @@
-import ProjectBox from "./components/project.js"
+const main = document.querySelector('#content')
 
-var main = document.querySelector('main')
+class Email{
+    send(){
+        const from_name = document.getElementById('from_name')
+        const from_email = document.getElementById('from_email')
+        const message = document.getElementById('message')
+        const message_status = document.querySelector('.message_status')
 
-function sendMail(){
+        if(from_name.value&&from_email.value&&message.value){
 
-    var from_name = document.getElementById('from_name')
-    var from_email = document.getElementById('from_email')
-    var message = document.getElementById('message')
-    var message_status = document.querySelector('.message-status')
+            message_status.innerHTML = 'Sending!!'
 
-    if(from_name.value&&from_email.value&&message.value){
+            const tempParams = {
+                from_name: from_name.value,
+                from_email: from_email.value,
+                message: message.value,
+            }
 
-        message_status.innerHTML = 'Sending!!'
+            emailjs.send("service_ygd6m6q","template_zgc8t6b",tempParams,)
+            .then( () => {
+                from_name.value = ''
+                from_email.value = ''
+                message.value = ''
+                message_status.innerHTML = "&#10003 message sent!!"
+            });
 
-        var tempParams = {
-            from_name: from_name.value,
-            from_email: from_email.value,
-            message: message.value,
-        }
-
-        emailjs.send('service_mym8wyj','template_zgc8t6b',tempParams)
-        .then(function(res){
-            from_name.value = ''
-            from_email.value = ''
-            message.value = ''
-            message_status.innerHTML = "&#10003 message sent!!"
-        },function(error){
-            alert('Error Sending Message!!',error)
-        })
-
-    }else{
-        if(!from_name.value){
-            from_name.classList.add('border-red-700')
         }else{
-            from_name.classList.remove('border-red-700')
-        }
+            if(!from_name.value){
+                from_name.classList.add('w3-border','w3-border-red')
+            }else{
+                from_name.classList.remove('w3-border','w3-border-red')
+            }
 
-        if(!from_email.value){
-            from_email.classList.add('border-red-700')
-        }else{
-            from_email.classList.remove('border-red-700')
-        }
+            if(!from_email.value){
+                from_email.classList.add('w3-border','w3-border-red')
+            }else{
+                from_email.classList.remove('w3-border','w3-border-red')
+            }
 
-        if(!message.value){
-            message.classList.add('border-red-700')
-        }else{
-            message.classList.remove('border-red-700')
+            if(!message.value){
+                message.classList.add('w3-border','w3-border-red')
+            }else{
+                message.classList.remove('w3-border','w3-border-red')
+            }
         }
     }
 }
 
-function loadHome(){
-    fetch('home.html').then((response)=>{
-        return response.text()
-    }).then(function(data){
-        main.innerHTML = data
+class Sidebar{
+    constructor(){
+        // Get the Sidebar
+    this.mySidebar = document.getElementById("mySidebar");
+    // Get the DIV with overlay effect
+    this.overlayBg = document.getElementById("myOverlay");
+    }
+    execute(){
+        const open = document.querySelector('.open_sidebar');
+        const close = document.querySelector('.close_sidebar');
 
-        Metz('#sub').animateText({
-            text: 'Fullstack Web Developer',
-        },20)
+        open.addEventListener('click',() => {
+            this.open()
+        });
 
-        Metz('#card').animateColor({
-            color: 'random',
-            type:'border'
-        },500)
+        close.addEventListener('click',() => {
+            this.close()
+        });
+    }
+    open(){
+        if (this.mySidebar.style.display === 'block') {
+            this.mySidebar.style.display = 'none';
+            this.overlayBg.style.display = "none";
+        } else {
+            this.mySidebar.style.display = 'block';
+            this.overlayBg.style.display = "block";
+        }
+    }
+    close(){
+        this.mySidebar.style.display = "none";
+        this.overlayBg.style.display = "none";
+    }
+}
 
-        Metz('.introduction').animateChar({
-            effect: 'random',
-            speed: 10
+class App{
+
+    home(){
+        const home = document.querySelector('.home');
+        home.addEventListener('click',() => {
+            this.loadHome();
+            window.scrollTo(top)
+            sidebar.close();
+        });
+    }
+
+    project(){
+        const home = document.querySelector('.project');
+        home.addEventListener('click',() => {
+            this.loadProject();
+            window.scrollTo(top)
+            sidebar.close()
+        });
+    }
+
+    about(){
+        const home = document.querySelector('.about');
+        home.addEventListener('click',() => {
+            this.loadAbout();
+            window.scrollTo(top)
+            sidebar.close();
+        });
+    }
+
+    loadHome(){
+        fetch('home.html')
+        .then( response => response.text())
+        .then( data => {
+            main.innerHTML = data
+            Metz('#sub').animateText({
+                text: 'Backend Web Developer',
+            },20)
+    
+            Metz('#card').animateColor({
+                color: 'random',
+                type:'border'
+            },500)
+    
+            Metz('.introduction').animateChar({
+                effect: 'random',
+                speed: 10
+            })
+    
         })
+    }
 
-    })
-}
-
-function loadProject(){
-    fetch('project.html').then((response)=>{
-        return response.text()
-    }).then(function(data){
-        main.innerHTML = data
-
-        var image = document.querySelectorAll('.image')
-        var imageShow = document.querySelector('.imageShow')
-        var imageShowModal = document.querySelector('.imageShowModal')
-        var hideModal = document.getElementsByClassName('hideModal')
-
-        hideModal.position = 'fixed'
-
-        var clickImage = (e)=>{
-            imageShowModal.style.display = 'block'
-            imageShow.src = e.target.src
-            imageShow.width = innerWidth - 50
-            imageShow.height = innerHeight - 20
-        }
-
-        var clickHideModalButton = (e)=>{
-            imageShowModal.style.display = 'none'
-        }
-
-        for(var i = 0; i < image.length; i++){
-            image[i].addEventListener('click',clickImage,false)
-        }
-
-        for(var i = 0; i<hideModal.length; i++){
-            hideModal[i].addEventListener('click',clickHideModalButton,false)
-        }
-
-    })
-}
-
-function loadAbout(){
-    fetch('about.html').then((response)=>{
-
-        return response.text() 
-
-    }).then((data)=>{
-
-        main.innerHTML = data
-
-        var sendEmail = document.getElementById('sendEmail')
-        sendEmail.addEventListener('click',()=>{
-            sendMail()
+    loadProject(){
+        fetch('project.html')
+        .then( response => response.text())
+        .then(function(data){
+            main.innerHTML = data
+    
+            const image = document.querySelectorAll('.image')
+            const imageShow = document.querySelector('.imageShow')
+            const imageShowModal = document.querySelector('.imageShowModal')
+            const hideModal = document.getElementsByClassName('hideModal')
+    
+            hideModal.position = 'fixed'
+    
+            const clickImage = (e)=>{
+                imageShowModal.style.display = 'block'
+                imageShow.src = e.target.src
+                imageShow.width = innerWidth - 50
+                imageShow.height = innerHeight - 20
+            }
+    
+            const clickHideModalButton = (e)=>{
+                imageShowModal.style.display = 'none'
+            }
+    
+            for(const i = 0; i < image.length; i++){
+                image[i].addEventListener('click',clickImage,false)
+            }
+    
+            for(const i = 0; i<hideModal.length; i++){
+                hideModal[i].addEventListener('click',clickHideModalButton,false)
+            }
+    
         })
+    }
 
-    })
+    loadAbout(){
+        fetch('about.html')
+        .then( response => response.text())
+        .then((data)=>{
+            main.innerHTML = data
+            const sendEmail = document.querySelector('form[name=sendEmail]')
+            sendEmail.addEventListener('submit',(e)=>{
+                e.preventDefault();
+                email.send();
+            })
+    
+        })
+    }
 }
 
-window.addEventListener('load',function(){
-    Metz('.home').addClass('text-blue-700')
-    loadHome()
-})
+const app = new App;
+const sidebar = new Sidebar;
+const email = new Email;
 
-const navbar = () => {
-    const trigger = document.querySelector('#navbarTrigger')
-    const list = document.querySelector('#navbarList')
-    trigger.addEventListener('click',() => {
-        list.classList.toggle('hidden')
-    })
+app.home();
+app.project();
+app.about();
+app.loadHome();
 
-    Metz('.home').on('click',function(){
-        Metz('.about').removeClass('text-blue-700')
-        Metz('.project').removeClass('text-blue-700')
-        Metz('.home').addClass('text-blue-700')
-        loadHome()
-    })
-    Metz('.project').on('click',function(){
-        Metz('.about').removeClass('text-blue-700')
-        Metz('.home').removeClass('text-blue-700')
-        Metz('.project').addClass('text-blue-700')
-        loadProject()
-        window.scrollTo(top)
-    })
-    Metz('.about').on('click',function(){
-        Metz('.project').removeClass('text-blue-700')
-        Metz('.home').removeClass('text-blue-700')
-        Metz('.about').addClass('text-blue-700')
-        loadAbout()
-        window.scroll(top)
-    })
-    
-    Metz('.home').on('click',function(){
-        Metz('#navbarList').toggleClass('hidden')
-    })
-    Metz('.project').on('click',function(){
-        Metz('#navbarList').toggleClass('hidden')
-    
-    })
-    Metz('.about').on('click',function(){
-        Metz('#navbarList').toggleClass('hidden')
-    
-    })
-}
-
-navbar()
+sidebar.execute();
