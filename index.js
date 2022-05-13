0a921e314489e9bb1db2fbfb88b6e2fe00930181
erselmetz <magbanuaersel@gmail.com>
2022-05-13 09:11:14 +0800

    'update'

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use('/style',express.static('./src/css/style.css'))
-app.use('/w3css',express.static('./src/css/w3.css'))
-app.use('/js/metz',express.static('./src/js/metz.js'))
-app.use('/js/core',express.static('./src/js/core.js'))
+app.use('/style',express.static(path.join(__dirname,'./src/css/style.css')))
+app.use('/w3css',express.static(path.join(__dirname,'./src/css/w3.css')))
+app.use('/js/metz',express.static(path.join(__dirname,'./src/js/metz.js')))
+app.use('/js/core',express.static(path.join(__dirname,'./src/js/core.js')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'./src/index.html'));
