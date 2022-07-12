fd7f0a052868f8cdf44e25f7caafa80845e160a0
erselmetz <magbanuaersel@gmail.com>
2022-07-12 14:07:00 +0800

    update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use('/style',express.static(path.join(__dirname,'./src/css/style.css')))
 app.use('/w3css',express.static(path.join(__dirname,'./src/css/w3.css')))
 app.use('/js/metz',express.static(path.join(__dirname,'./src/js/metz.js')))
 app.use('/js/core',express.static(path.join(__dirname,'./src/js/core.js')))
+app.use('/img/simple-calculator',express.static(path.join(__dirname,'./src/img/Simple-calculator.png')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'./src/index.html'));
