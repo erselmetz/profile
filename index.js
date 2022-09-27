53bd84a316646710844f3cb4e255ac396e43abcf
erselmetz <magbanuaersel@gmail.com>
2022-09-27 08:43:40 +0800

    modified routes

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ app.use('/style',express.static(path.join(__dirname,'./src/css/style.css')))
 app.use('/w3css',express.static(path.join(__dirname,'./src/css/w3.css')))
 app.use('/js/metz',express.static(path.join(__dirname,'./src/js/metz.js')))
 app.use('/js/core',express.static(path.join(__dirname,'./src/js/core.js')))
+app.use('/js/jquery',express.static(path.join(__dirname, './src/js/jquery.js')));
 
 // img
 app.use('/img/simple-calculator',express.static(path.join(__dirname,'./src/img/Simple-calculator.png')))
 app.use('/img/note-app-javascript',express.static(path.join(__dirname,'./src/img/Note-App-Javascript.png')))
 app.use('/img/login-system',express.static(path.join(__dirname,'./src/img/Screenshot 2022-09-09 161657.png')))
+
+// jQueryScript
+app.use('/jQueryScript/aeo-typewriter/css',express.static(path.join(__dirname,'./src/jQueryScript/aeo-typewriter/css/aeo-style.css')))
+app.use('/jQueryScript/aeo-typewriter/js',express.static(path.join(__dirname,'./src/jQueryScript/aeo-typewriter/js/aeo-typewriter.js')))
 
 // routes
 app.get('/', (req, res) => {
